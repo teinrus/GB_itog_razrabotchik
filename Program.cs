65dc8219b406d305b7f9af7b1554c23940acc04d
Novichenko Evgeniy <tnrkip@yande.ru>
2023-05-02 11:57:08 +0300

    add algoritm

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string [] Foo(string [] arr){
+    string [] arrTemp = new string[arr.Length];
+    int j=0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+         if (arr[i].Length <=3){
+            arrTemp[j]=arr[i];
+            j++;    
+        }
+    }
+    return arrTemp;
+
+}
+string[] arr = {"1234", "156", "-2", "computer science"};
+
+System.Console.WriteLine(String.Join(",", Foo(arr)));
